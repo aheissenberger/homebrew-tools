6f4cb0d71d921be9e7f0306ba0b580f8eab7d9b7
Andreas Heissenberger <andreas@heissenberger.at>
2026-03-02 09:41:24 +0000

    fix: brew audit --strict compliance (desc article, license, depends_on)

--- Formula/gitvault.rb
+++ Formula/gitvault.rb
@@ -1,8 +1,8 @@
 class Gitvault < Formula
-  desc "A secure, distributed vault for managing secrets and sensitive data with Git"
+  desc "Secure, Git-native vault for managing age-encrypted secrets"
   homepage "https://github.com/aheissenberger/gitvault"
+  license "MIT"
   version "0.2.18"
-  depends_on :macos
 
   on_macos do
     on_arm do
